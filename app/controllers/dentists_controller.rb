99eee1e4f56a6cbca367703a51e04d9c26a470d1
Kim G <xiaole.guo@yahoo.com>
2022-03-25 23:25:47 -0600

    complete story 12 dentist create action

--- app/controllers/dentists_controller.rb
+++ app/controllers/dentists_controller.rb
@@ -10,4 +10,18 @@ class DentistsController < ApplicationController
 
   def new
   end
+
+  def create
+    Dentist.create(dentist_params)
+    redirect_to '/dentists'
+  end
+
+
+
+private
+
+  def dentist_params
+    params.permit(:name, :max_patient_capacity, :accepting_new_patient, :hours, :rating)
+  end
+
 end 
